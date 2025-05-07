2326bd3549221cbc7c011adc9bececf8786ed35b
Marina Lampert <marinafabienne.lampert@gmail.com>
2025-05-07 12:20:21 +0200

    update 7.5.25 12:20

---
+++ js/script.js
@@ -0,0 +1,12 @@
+async function loadData() {
+    const url = 'https://api.com'; // mit korrekter API-URL ersetzen
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const data = await loadData();
+console.log(data); // gibt die Daten der API oder false in der Konsole aus
